58d7e73897f97bea6ca40ee9911d0e46b936d204
Mahmoud Harding <55882955+mahmoudharding@users.noreply.github.com>
2026-04-04 08:11:39 -0400

    Update guide and gss data

--- get_data.py
+++
@@ -1,8 +0,0 @@
-# Download the data for the project
-from pathlib import Path as P
-import urllib.request as u
-
-p = P.home()/'.local/share/datasets/hsclass1.txt'
-p.parent.mkdir(parents=True, exist_ok=True)
-p.exists() or u.urlretrieve('https://raw.githubusercontent.com/ds4e-org/ap-cs-dsc/refs/heads/main/data/hsclass1.txt')
-print(f"Data now avaialbel at {p}")
